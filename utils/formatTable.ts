00908b9963388ea9c7ec9bf816b4fe7ca68a9633
fussy113 <s1220080@gmail.com>
2020-03-11 23:00:12 +0900

    陽性患者の属性テーブル変更

--- utils/formatTable.ts
+++ utils/formatTable.ts
@@ -2,27 +2,27 @@ import dayjs from 'dayjs'
 
 const headers = [
   { text: '日付', value: '日付' },
+  { text: '国籍', value: '国籍' },
   { text: '居住地', value: '居住地' },
-  { text: '年代', value: '年代' },
-  { text: '性別', value: '性別' },
-  { text: '退院※', value: '退院', align: 'center' }
+  { text: '年代・性別', value: '年代・性別' },
+  { text: '備考', value: '備考' },
 ]
 
 type DataType = {
   リリース日: Date
+  国籍: string | null
   居住地: string | null
-  年代: string | null
-  性別: '男性' | '女性'
-  退院: '◯' | null
+  '年代・性別': string | null
+  備考: string | null
   [key: string]: any
 }
 
 type TableDataType = {
   日付: string
   居住地: DataType['居住地']
-  年代: DataType['年代']
-  性別: DataType['性別'] | '不明'
-  退院: DataType['退院']
+  国籍: DataType['国籍']
+  '年代・性別': DataType['年代・性別']
+  備考: DataType['備考']
 }
 
 type TableDateType = {
@@ -37,11 +37,11 @@ export default (data: DataType[]) => {
   }
   data.forEach(d => {
     const TableRow: TableDataType = {
-      日付: dayjs(d['リリース日']).format('MM/DD') ?? '不明',
-      居住地: d['居住地'] ?? '不明',
-      年代: d['年代'] ?? '不明',
-      性別: d['性別'] ?? '不明',
-      退院: d['退院']
+      日付: dayjs(d['発表日']).format('MM/DD') ?? '不明',
+      国籍: d['国籍'] ?? '不明',
+      居住地: d['住居地'] ?? '不明',
+      '年代・性別': d['年代・性別'] ?? '不明',
+      備考: d['備考'] ?? '不明',
     }
     tableDate.datasets.push(TableRow)
   })
