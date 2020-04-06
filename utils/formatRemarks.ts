834d31caa7ee04905d8e6407eb6401bb6fbb8f63
amay077 <okuokuoku@gmail.com>
2020-04-06 21:26:36 +0900

    fix データ判定

--- utils/formatRemarks.ts
+++ utils/formatRemarks.ts
@@ -10,7 +10,7 @@ export default (data: DataType[]) => {
   const today = new Date()
   const joined = data
     .filter(d => new Date(d['日付']) < today)
-    .filter(d => d['合算'] !== null && d['合算'] !== '')
+    .filter(d => d['合算'])
     .map(d => convertDateToShortFormat(new Date(d['日付']).toISOString()))
     .join(',')
   return joined !== '' ? `※${joined}は合算値(出典参照)` : ''
