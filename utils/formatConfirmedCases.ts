7b778065201f558e18382305420df2d335a0f997
daikiyamamuro <daikiyamamuro@gmail.com>
2020-04-15 23:42:14 +0900

    「検査陽性者の状況」に「施設入所」項目を追加

--- utils/formatConfirmedCases.ts
+++ utils/formatConfirmedCases.ts
@@ -1,63 +1,24 @@
-type DataType = {
-  attr: '検査実施人数'
-  value: number
-  children: [
-    {
-      attr: '陽性患者数'
-      value: number
-      children: [
-        {
-          attr: '入院中'
-          value: number
-          children: [
-            {
-              attr: '軽症・中等症'
-              value: number
-            },
-            {
-              attr: '重症'
-              value: number
-            }
-          ]
-        },
-        {
-          attr: '退院'
-          value: number
-        },
-        {
-          attr: '転院'
-          value: number
-        },
-        {
-          attr: '死亡'
-          value: number
-        }
-      ]
-    }
-  ]
-}
-
-type ConfirmedCasesType = {
+interface History {
+  更新日時: string
   検査実施人数: number
-  陽性物数: number
+  陽性患者数: number
   入院中: number
   軽症中等症: number
   重症: number
+  施設入所: number
+  退院: number
   転院: number
   死亡: number
-  退院: number
+  備考: string
+}
+
+type DataType = {
+  date: string
+  data: History[]
 }
 
 export default (data: DataType) => {
-  const formattedData: ConfirmedCasesType = {
-    検査実施人数: data.value,
-    陽性物数: data.children[0].value,
-    入院中: data.children[0].children[0].value,
-    軽症中等症: data.children[0].children[0].children[0].value,
-    重症: data.children[0].children[0].children[1].value,
-    転院: data.children[0].children[2].value,
-    死亡: data.children[0].children[3].value,
-    退院: data.children[0].children[1].value
-  }
-  return formattedData
+  const length = data.data.length
+  const latest = data.data[length - 1]
+  return latest
 }
