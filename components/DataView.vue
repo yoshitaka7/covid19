56ecdfd75a329b321352038530f65249ecc3d558
hmatsu47 <hmatsu47@gmail.com>
2020-05-02 17:15:15 +0900

    「出典」の下寄せ方法が間違っていたので再修正

--- components/DataView.vue
+++ components/DataView.vue
@@ -20,7 +20,7 @@
     >
       <slot />
     </v-card-text>
-    <v-footer class="DataView-Footer">
+    <v-footer absolute class="DataView-Footer">
       <time :datetime="formattedDate">{{ date }} 更新</time>
       <a
         v-if="url"
@@ -146,9 +146,5 @@ export default class DataView extends Vue {
 }
 .v-toolbar__content {
   height: auto !important;
-}
-.v-footer {
-  position: absolute;
-  bottom: 0;
 }
 </style>
