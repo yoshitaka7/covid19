e3c31cd986f8f24f3f76d367a17aace1973da738
amay077 <okuokuoku@gmail.com>
2020-05-24 18:34:32 +0900

    検査陽性者状況の推移から「日別」のボタン消した

--- components/MainSummaryChart.vue
+++ components/MainSummaryChart.vue
@@ -88,7 +88,7 @@ export default class MainSummaryChart extends Vue {
     '凡例をクリックするとその項目の[表示/非表示]が切り替えられます'
   ]
 
-  private readonly showSelector = true
+  private readonly showSelector = false
   private readonly dataKind: DataKind = 'daily-transition'
   private readonly dataKinds = [
     { key: 'daily-transition', label: '日別' } as SelectorItem
