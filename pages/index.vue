02e8b2af8733b2b487df65b266679a0d4ad3eedc
amay077 <okuokuoku@gmail.com>
2021-01-16 20:28:40 +0900

    国による緊急事態宣言（2回目）が出された事を表示

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,11 @@
     />
     <whats-new
       class="mb-1"
+      text="1月14日、緊急事態宣言が発出されました（対象期間：1/14～2/7）"
+      url="https://www.pref.aichi.jp/site/covid19-aichi/covid19-aichi.html"
+    />
+    <whats-new
+      class="mb-1"
       :text="newsItem.text"
       :date="newsItem.date"
       :url="newsItem.url"
