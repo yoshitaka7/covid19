30897f25bdc24f37038ad9b6261d2bab449dabc9
yoshitaka taniguchi <truck_mos1500@yahoo.co.jp>
2022-06-21 22:21:51 +0900

    グラフインジケーター修正

--- components/DateSelectSlider.vue
+++ components/DateSelectSlider.vue
@@ -1,22 +1,34 @@
 <template>
-  <v-range-slider
-    v-model="sliderValue"
-    :value="sliderValue"
-    :label="'表示期間'"
-    :min="min"
-    :max="max"
-    thumb-label="always"
-    style="padding-top: 35px;"
-    color="rgba(189, 63, 76, 1)"
-    track-color="rgba(189, 63, 76, 0.3)"
-  >
-    <template v-slot:thumb-label="props">
-      {{ getSliderLabels(props.value) }}
-    </template>
-  </v-range-slider>
+  <div>
+    <v-range-slider
+      v-model="sliderValue"
+      :value="sliderValue"
+      :label="'表示期間'"
+      :min="min"
+      :max="max"
+      style="padding-top: 35px;"
+      color="rgba(189, 63, 76, 1)"
+      track-color="rgba(189, 63, 76, 0.3)"
+    >
+    </v-range-slider>
+    <div class="range-slider-label">
+      <output>{{showSliderLabels(value[0])}}から</output>
+      <output>{{showSliderLabels(value[1])}}まで</output>
+    </div>
+  </div>
 </template>
 
+<style lang="scss">
+  .range-slider-label {
+    width: 90%;
+    margin: -15px 0 0 auto;
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
+
 <script>
+import dayjs from 'dayjs'
 export default {
   name: 'DateSelectSlider',
   props: {
@@ -58,15 +70,19 @@ export default {
     }
   },
   methods: {
-    getSliderLabels(index) {
-      if (index < this.chartData.length) {
-        return this.labelFormatter(
-          this.chartData[index],
-          this.sliderValue[0] === index
-        )
-        // return this.chartData[index].label
+    showSliderLabels(order) {
+      const isFrom =  this.sliderValue[0] === order
+      console.log(isFrom)
+      if (order < this.chartData.length) {
+        const certainPeriod = this.chartData
+        const particularDate = certainPeriod[order]
+        if (typeof particularDate === 'string') {
+          return dayjs(particularDate).format('YYYY年M月D日')
+        } else {
+          return  particularDate.map(date => dayjs(date).format('YYYY年M月D日'))[isFrom ? 0 : 1]
+        }
       }
-      return this.chartData[this.chartData.length - 1]?.label ?? ''
+      return ''
     }
   }
 }
